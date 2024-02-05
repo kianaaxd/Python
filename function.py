b65491bee40eaadb35ede85f6d17849041d5895c
kianaaxd <lancemirano01@gmail.com>
2024-02-05 12:27:22 -0700

    .

---
+++ function.py
@@ -0,0 +1,17 @@
+def greet():
+    print("Welcome to RDP")
+
+greet()
+
+
+def sum(x,y):
+    print("sum of the numbers = ",x+y)
+
+sum(5,7)
+
+def sum(x,y):
+    z = x+y
+    return z
+
+s = sum(5,7)
+print("The sum of the two numbers = ",s)
