fe5d51a83d769a898870ae3307139f0b38171d41
kianaaxd <lancemirano01@gmail.com>
2024-02-16 09:32:33 -0700

    .

--- squareroot.py
+++ squareroot.py
@@ -1,13 +1,13 @@
 import math
 
-def variable(a, b):
+def readingbreak(a, b):
     c = math.sqrt (a**2 + b**2)
     return c
 
 a = int(input('Enter the value of a: '))
 b = int(input('Enter the value of b: '))
 
-c = variable(a,b)
+c = readingbreak(a,b)
 
 print('The square root is', c)
 
